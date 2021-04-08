272d335feb0b0a011da9342dd9b2e03c50a426b9
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-04-08 14:48:39 +0300

    Updates

---
+++ precache-manifest.5bc41144dac923819f909224f6d59bfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75450510448d140dac2ca70c215ef2fa",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "887c9994309bd827f267",
+    "url": "/ToDoList/static/css/main.fd830398.chunk.css"
+  },
+  {
+    "revision": "fe7ff3dace87d4c9d668",
+    "url": "/ToDoList/static/js/2.8acf38a4.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/ToDoList/static/js/2.8acf38a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887c9994309bd827f267",
+    "url": "/ToDoList/static/js/main.08c4867b.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
