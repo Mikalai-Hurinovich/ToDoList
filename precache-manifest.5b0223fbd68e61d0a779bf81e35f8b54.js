5709c8e9cbecceff6a9e6a6a3ff9c8886e1fcbbb
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-04-15 08:24:56 +0300

    Updates

--- precache-manifest.5b0223fbd68e61d0a779bf81e35f8b54.js
+++ precache-manifest.5b0223fbd68e61d0a779bf81e35f8b54.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "75450510448d140dac2ca70c215ef2fa",
+    "revision": "897a86239228122898b27db14cf7f6ab",
     "url": "/ToDoList/index.html"
   },
   {
-    "revision": "887c9994309bd827f267",
+    "revision": "ffc1511f4e7915949a73",
     "url": "/ToDoList/static/css/main.fd830398.chunk.css"
   },
   {
-    "revision": "fe7ff3dace87d4c9d668",
-    "url": "/ToDoList/static/js/2.8acf38a4.chunk.js"
+    "revision": "5a24e571c0a9390d9498",
+    "url": "/ToDoList/static/js/2.099d5802.chunk.js"
   },
   {
     "revision": "d2266803016540034565984c8bb59069",
-    "url": "/ToDoList/static/js/2.8acf38a4.chunk.js.LICENSE.txt"
+    "url": "/ToDoList/static/js/2.099d5802.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "887c9994309bd827f267",
-    "url": "/ToDoList/static/js/main.08c4867b.chunk.js"
+    "revision": "ffc1511f4e7915949a73",
+    "url": "/ToDoList/static/js/main.fd26c7f3.chunk.js"
   },
   {
     "revision": "448f1ac8991118963dc3",
