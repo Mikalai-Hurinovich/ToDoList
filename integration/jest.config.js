e80456bb8d93d6accaddec66477f714311e69170
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-04-24 08:32:02 +0300

    add: storyBook + snapshot + testTimeout

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
     preset: 'jest-puppeteer',
     testRegex: './*\\.test\\.js$',
     setupFilesAfterEnv: ['./setupTests.js'],
-    testTimeout: 10000
+    testTimeout: 10000 // add testTimeout, cause may be problem
 };
