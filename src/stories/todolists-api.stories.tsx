db318decd92ee2dcf05e86d54782e5e5849f4d6c
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-05-01 10:16:16 +0300

    feat: add axios lib, work with SB

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,22 +1,30 @@
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
     title: 'API'
 }
+const settings = {
+    withCredentials: true,
+    'API-KEY': 'ba71bc47-1781-4fe9-b781-f4d80c7b2c13'
+}
 
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
-    useEffect(() => {
-        // здесь мы будем делать запрос и ответ закидывать в стейт.
-        // который в виде строки будем отображать в div-ке
 
-    }, [])
+    useEffect(() => {
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings
+        ).then(res => {
+            setState(res.data)
+        })
+    }, [state])
 
     return <div> {JSON.stringify(state)}</div>
 }
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
